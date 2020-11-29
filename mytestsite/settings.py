5f4b89f3e91484e98c7343a61c1ad7a78a6ae189
unknown <nguyennhubaosang@gmail.com>
2020-11-29 21:38:08 +0700

    a

--- mytestsite/settings.py
+++ mytestsite/settings.py
@@ -123,9 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'webtest/static')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'webtest/static')]
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
