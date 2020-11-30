749958e6d8d1fb272f9dbc39a55cb01d5bcd5b2f
unknown <nguyennhubaosang@gmail.com>
2020-11-30 17:24:59 +0700

    a

--- mytestsite/settings.py
+++ mytestsite/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
